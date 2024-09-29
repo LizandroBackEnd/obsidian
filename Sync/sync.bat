3667acdf42a0016eafe4f16cde25651e1e3f0b23
LizandroBackend <codeversebackend@gmail.com>
2024-09-28 23:42:35 -0600

    build(programming): Programming the file sync for make push´s

--- Sync/sync.bat
+++ Sync/sync.bat
@@ -1,7 +1,18 @@
 @echo off
-cd "C:\Users\Lizandro\Documents\Obsidian Vault" 
- 
-git pull origin main 
-git add .
-git commit -m "build(sync): sync vault" 
-git push origin main
+cd "C:\Users\Lizandro\Documents\Obsidian Vault"
+
+REM Get the latest changes from the remote repository
+git fetch origin main 
+
+REM Check for changes between local branch and remote branch
+FOR /F "tokens=*" %%i IN ('git diff --name-only origin/main') DO (
+    REM If there are changes, pull, add and push
+    git pull origin main
+    git add .
+    git commit -m "build(sync): sync vault"
+    git push origin main
+    GOTO END
+)
+
+echo No changes to sync.
+:END
